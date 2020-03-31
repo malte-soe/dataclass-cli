463160d94088c24da8f58de7c0c5d2027b4e333f
Malte Sönnichsen <chacki@users.noreply.github.com>
2020-03-31 19:40:46 +0200

    Added setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dc-cli",  # Replace with your own username
+    version="0.0.1",
+    author="Malte Soennichsen",
+    author_email="malte-soe@users.noreply.github.com",
+    description="Dataclass command line interface",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/malte-soe/dataclass-cli",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
