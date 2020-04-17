3f8ac908ec4d6e3d873637fa32a13e6fbd623693
Malte Sönnichsen <8942015+malte-soe@users.noreply.github.com>
2020-04-17 21:39:04 +0200

    Prepare v0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dc-cli",
-    version="0.0.3",
+    version="0.0.4",
     author="Malte Soennichsen",
     author_email="malte-soe@users.noreply.github.com",
     description="Dataclass command line interface",
